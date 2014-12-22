1bdf5ea97b1b09c6c6c6797f9a0912c5deef800c
Geddy05 <geddyschellevis@gmail.com>
2014-12-22 13:50:24 +0100

    klantinformatie en bestelInformatie toegevoegd

---
+++ src/algoritmiek/BestelInformatie.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritmiek;
+
+/**
+ *
+ * @author geddyS
+ */
+public class BestelInformatie {
+    public int KlantId;
+    public int BestellingId;
+    public boolean isVerwerkt;
+    public String startTijd;
+    public String Duur;
+    public boolean isFinished;
+    public boolean isWaiting;
+    
+}
