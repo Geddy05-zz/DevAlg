429b02aa80a7c2de0c3730d05b7f5e1968787460
Geddy05 <geddyschellevis@gmail.com>
2014-12-22 14:42:00 +0100

    vul array toegevoegd met dummy gegevens

--- src/algoritmiek/klantGegevensBestand.java
+++ src/algoritmiek/klantGegevensBestand.java
@@ -11,10 +11,11 @@ package algoritmiek;
  */
 public class klantGegevensBestand {
     
-    private KlantInformatie KlantArray[];
+    public KlantInformatie KlantArray[];
     
     public void vulArray()
     {
+        KlantArray = new KlantInformatie[5]; // create the length of the array
         KlantArray[0] = new KlantInformatie(00001,"Bruin","De","Laura",22,"Vrouw","De lier","laura@laura.nl");
         KlantArray[1] = new KlantInformatie(00002,"Schellevis","Geddy",24,"Man","Maasluis","geddy@geddy.nl");
         KlantArray[2] = new KlantInformatie(00003,"Vink","Manasse",23,"Man","Maasluis","vink@vink.nl");
